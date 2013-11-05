4585ad5e546f3296095784d01d23d40c5cc0ebdd
scilence <scilence.wang@gmail.com>
2013-11-05 11:28:40 +0800

    Add: Helper.js

---
+++ Helper.js
@@ -0,0 +1,27 @@
+;
+(function($){
+    "use strict";
+    $.extend($.fn, {
+        check: function (b) {
+            if (b === undefined) {
+                b = true;
+            }
+            return this.each(function () {
+                this.checked = b;
+            });
+        },
+        isNumber: function(n) {
+            return !isNaN(parseFloat(n)) && isFinite(n);
+        },
+        trimObj: function trimCriteria(o) {
+            if (!o) { return {}; }
+
+            for (var t in o) {
+                if (o.hasOwnProperty(t)) {
+                    o[t] = $.trim(o[t]);
+                }
+            }
+            return o;
+        }
+    });
+})
